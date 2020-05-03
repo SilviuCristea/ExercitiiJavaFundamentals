b5a4798948ef72b9eeea07477594438397402aed
unknown <silviu.cristea@ymail.com>
2020-05-03 14:14:02 +0300

    Inceput

---
+++ Exercitii1-7/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // se da un sir de 7 cuvinte
+                //sa se concateneze si sa se afiseze cuvintele de pe pozitiile divizibile cu 3 si mai mari decat 0
+
+        //o functie care primeste 2 striguri ca parametri si verifica daca al 2 lea string exista in primul string
+
+        // sa se gaseasca cea mai mare valoare dintr-un vector
+            // avem un vector de 10 elemnente intregi
+
+        // se da un text ; sa se numere cuvintele care au lungimea impara si se termina cu litera e
+
+        //Sa se numere toate cuvintele dintr un text care au lungimea mai mare decat 2, contin caraterul "i" si acesta se afla pe o pozitie para
+
+        // sa se afiseze toate cuvintele dintr un text care incep cu litera s sau cu litera d
+            // sa se afiseze litera de la pozitia 3 din fiecare cuvant
+
+        //Se da un sir de caractere
+            //Sa se afiseze in ordinea inversa
+
+    }
+}
