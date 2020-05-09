6328e343cd5a14f503b0160bd3c36a0beb09f9b1
unknown <silviu.cristea@ymail.com>
2020-05-09 11:15:30 +0300

    Calculator stiintific 1

---
+++ ExercitiuCalculator_09Mai/src/CalculatorStiintific.java
@@ -0,0 +1,24 @@
+public class CalculatorStiintific extends Calculator {
+
+    public CalculatorStiintific(){
+
+    }
+    // Un numar ridicat la puterea altui numar
+    public double ridicareLaPutere(int a, int b){
+       return Math.pow(a,b);
+    }
+    // Radical dintr-un numar
+    public double radical(int a){
+        return Math.sqrt(a);
+    }
+    // Logaritm in baza 10 dintr-un numar
+    public double logaritm10(int a){
+        return Math.log10(a);
+    }
+    // Logaritm dintr-un numar
+    public double logaritm(int a){
+        return Math.log(a);
+    }
+
+
+}
--- ExercitiuCalculator_09Mai/src/Main.java
+++ ExercitiuCalculator_09Mai/src/Main.java
@@ -47,9 +47,59 @@ public class Main {
 
         System.out.println("Rezultatul impartirii este: "+calculator.Impartire(primulNumar, alDoileaNumar));
 
+        // Sa se creeze un calculator stiintific care sa faca si urmatoarele operatii:
+            // Un numar ridicat la puterea altui numar
+            // Radical dintr-un numar
+            // Logaritm in baza 10 dintr-un numar
+            // Logaritm dintr-un numar
 
+        //Metoda 1 - afisare directa
 
+        CalculatorStiintific calculatorStiintific=new CalculatorStiintific();
 
+        System.out.println("Ridicare la putere: "+calculatorStiintific.ridicareLaPutere(2, 3));
+
+        System.out.println("Radicalul este: "+calculatorStiintific.radical(9));
+
+        System.out.println("Logaritm in baza 10 este: "+calculatorStiintific.logaritm10(22));
+
+        System.out.println("Logaritul este: "+calculatorStiintific.logaritm(22));
+
+        //Metoda 2
+
+        //Declaram o variabila pentru ridicarea la putere si apelam variabila
+
+        double ridicareLaPutere=calculatorStiintific.ridicareLaPutere(2, 3);
+
+        System.out.println("Rezultatul ridicarii la putere este: "+ridicareLaPutere);
+
+        // Metoda 3 - cu citire de la tastatura
+
+        Scanner scanner1= new Scanner(System.in);
+
+        System.out.println("Va rugam introduceti primul numar pentru ridicare la putere");
+
+        primulNumar=scanner1.nextInt();
+
+        System.out.println("Va rugam introduceti al doilea numar pentru ridicare la putere");
+
+        alDoileaNumar=scanner1.nextInt();
+
+        System.out.println("Rezultatul ridicarii la putere al numerelor "+primulNumar+" "+alDoileaNumar+" este: "+calculatorStiintific.ridicareLaPutere(primulNumar, alDoileaNumar));
+
+        System.out.println("Va rugam sa introduceti numarul pentru calcularea radicalului");
+
+        primulNumar=scanner1.nextInt();
+
+        System.out.println("Rezultatul radicalului numarului "+primulNumar+" este: "+calculatorStiintific.radical(primulNumar));
+
+        System.out.println("Va rugam sa introduceti numarul pentru calcularea logaritmului in baza 10");
+
+        primulNumar=scanner1.nextInt();
+
+        System.out.println("Rezultatul log10 al numarului "+primulNumar+" este: "+calculatorStiintific.logaritm10(primulNumar));
+
+        System.out.println("Rezultatul logaritmului numarului "+primulNumar+" este: "+calculatorStiintific.logaritm(primulNumar));
 
 
     }
