9e6599ea632ba2706e1b735c4cd13477f70a8ec4
unknown <silviu.cristea@ymail.com>
2020-10-25 08:59:31 +0200

    Exercitiu Recap

--- Recapitulare24OctombrieEx2/src/main/java/Option.java
+++ Recapitulare24OctombrieEx2/src/main/java/Option.java
@@ -8,10 +8,13 @@ public class Option {
         System.out.println(subscription.getPrice());
 
         //Utilizatorul selecteaza Platinum
-        //Creem un obiect subscription pentru a nu modifica in toate clasele
+        //Creem un obiect subscription pentru a nu modifica in toate clasele in cazul in care oferta Platinum nu mai e valabila
 
         Subscription subscription1 = subscriptionFactory.getSubscription("Platinum");
         System.out.println(subscription1.getPrice());
+
+        Subscription subscription2 = subscriptionFactory.getSubscription("Basic");
+        System.out.println(subscription2.getPrice());
 
         /*BasicSubsription basicSubsription = (BasicSubsription) subscriptionFactory.getSubscription("Basic");
         System.out.println("Ati ales optiunea Basic. "+basicSubsription.getPrice());
